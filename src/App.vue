076f5149b0fa9ae94d7be831ebd693e39896d967
gagandeep_thapa <gagandeep.thapa@globalbilling.com>
2017-11-15 17:27:37 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <app-header v-bind:title="title"></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data () {
+    return {
+      ninjas: [
+          {name: 'Ryu', spaciality: 'Vue Components', show: false},
+          {name: 'Crystal', spaciality: 'HTML Wizardry', show: false},
+          {name: 'Hitoshi', spaciality: 'Click Events', show: false},
+          {name: 'Tango', spaciality: 'Conditionals', show: false},
+          {name: 'Kami', spaciality: 'Webpack', show: false},
+          {name: 'Yoshi', spaciality: 'Data Diggin', show: false},
+      ],
+      title: "Vue Ninjas",
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    },
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    background: lightgreen;
+    padding: 10px;
+}
+h1{
+    color: #222;
+    text-align: center;
+}
+</style>
